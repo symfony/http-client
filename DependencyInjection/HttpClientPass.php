26cc224ea7103dda90e9694d9e139a389092d007
Lctrs <jerome@prmntr.me>
2025-12-04 20:40:12 +0100

    [HttpClient] CachingHttpClient must run after UriTemplate and Scoping

--- DependencyInjection/HttpClientPass.php
+++ DependencyInjection/HttpClientPass.php
@@ -27,7 +27,7 @@ final class HttpClientPass implements CompilerPassInterface
 
         foreach ($container->findTaggedServiceIds('http_client.client') as $id => $tags) {
             $container->register('.debug.'.$id, TraceableHttpClient::class)
-                ->setDecoratedService($id, null, 5)
+                ->setDecoratedService($id, null, 100)
                 ->setArguments([new Reference('.inner'), new Reference('debug.stopwatch', ContainerInterface::IGNORE_ON_INVALID_REFERENCE), new Reference('profiler.is_disabled_state_checker', ContainerInterface::IGNORE_ON_INVALID_REFERENCE)])
                 ->addTag('kernel.reset', ['method' => 'reset']);
             $container->getDefinition('data_collector.http_client')
