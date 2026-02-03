2d0e9af27ad24edb2b0d2813ecfc18145253d14f
Nicolas Grekas <nicolas.grekas@gmail.com>
2026-02-03 15:18:24 +0100

    [HttpClient] Fix destructor throwing while timeout was handled

--- Response/TransportResponseTrait.php
+++ Response/TransportResponseTrait.php
@@ -45,6 +45,7 @@ trait TransportResponseTrait
     private \InflateContext|bool|null $inflate = null;
     private ?array $finalInfo = null;
     private ?LoggerInterface $logger = null;
+    private bool $didTimeout = false;
 
     public function getStatusCode(): int
     {
@@ -126,7 +127,7 @@ trait TransportResponseTrait
     {
         $this->shouldBuffer = true;
 
-        if ($this->initializer && null === $this->info['error']) {
+        if ($this->initializer && null === $this->info['error'] && !$this->didTimeout) {
             self::initialize($this);
             $this->checkStatusCode();
         }
@@ -183,6 +184,7 @@ trait TransportResponseTrait
                         unset($responses[$j]);
                         continue;
                     } elseif ($elapsedTimeout >= $timeoutMax) {
+                        $response->didTimeout = true;
                         $multi->handlesActivity[$j] = [new ErrorChunk($response->offset, \sprintf('Idle timeout reached for "%s".', $response->getInfo('url')))];
                         $multi->lastTimeout ??= $lastActivity;
                         $elapsedTimeout = $timeoutMax;
